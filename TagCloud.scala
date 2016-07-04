1c60aab7823c8d19ac9c6d840f8c9b28f35a7439
Daniel Taskoff <daniel.taskoff@gmail.com>
2016-07-04 10:11:07 +0300

    Get the minimum and maximum possible count of a word

--- TagCloud.scala
+++ TagCloud.scala
@@ -5,6 +5,10 @@ object TagCloud {
 
   def mostCommonWords(
       wordCount: Map[String, Int])(n: Int): List[String] =
+
     wordCount.toList.sortBy(_._2).map(_._1).reverse.take(n)
+
+  def range(wordCount: Map[String, Int]): (Int, Int) =
+    (wordCount.minBy(_._2)._2, wordCount.maxBy(_._2)._2)
 
 }
