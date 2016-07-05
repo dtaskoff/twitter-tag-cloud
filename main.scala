ee7d6f9bcc53bd698fffd5e8d0cd422dd17fc6b5
Daniel Taskoff <daniel.taskoff@gmail.com>
2016-07-05 13:36:52 +0300

    Start implementing a GUI

--- main.scala
+++ main.scala
@@ -4,26 +4,53 @@ import ttc.TStreamer._
 import ttc.TagCloud._
 
 import scala.collection.JavaConverters._
+import scala.swing._
+import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
 
+class DrawPanel(image: BufferedImage) extends Panel {
+  override def paintComponent(g: Graphics2D) =
+    if (image != null) {
+      val (w, h) = (image.getWidth(), image.getHeight())
+      val ratio = 640.0 / w.toDouble min 480.0 / h.toDouble
+      g.drawImage(image, 0, 0, null)
+      g.scale(ratio, ratio)
+    }
+}
+
+class TwitterTagCloudUI(onClose: () => Unit) extends MainFrame {
+  title = "Twitter tag cloud"
+  preferredSize = new Dimension(640, 480)
+  contents = new FlowPanel {
+    contents += new DrawPanel(null)
+  }
+}
+
 object TwitterTagCloud {
 
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
     val tstreamer = new TStreamer(1)
+    val ui = new TwitterTagCloudUI(tstreamer.close)
+    ui.visible = true
     tstreamer.stream()
-    Thread sleep 60000
+
+    Thread sleep 9000
     var wc = tstreamer.getWordCount().asScala.toMap.mapValues(_.toInt)
-    ImageIO.write(tagCloud(wordsWithSizes(mostCommonWords(3)(wc))),
-      "png", new File("01.png"))
+    println("the image is ready")
+    ui.contents = new FlowPanel {
+      contents += new DrawPanel(
+        tagCloud(wordsWithSizes(mostCommonWords(3)(wc))))
+    }
+    ui.repaint()
+  }
 
-    Thread sleep 60000
-    wc = tstreamer.getWordCount().asScala.toMap.mapValues(_.toInt)
-    ImageIO.write(tagCloud(wordsWithSizes(mostCommonWords(3)(wc))),
-      "png", new File("02.png"))
-
-    tstreamer.close()
+  def test(n: Int) = {
+    import ttc.StreamDummy._
+    ImageIO.write(
+      tagCloud(wordsWithSizes(mostCommonWords(n)(getWordCount))),
+      "png", new File("test.png"))
   }
 
 }
